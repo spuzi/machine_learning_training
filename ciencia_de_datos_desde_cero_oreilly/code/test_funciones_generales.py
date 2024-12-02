e5720a0feb75d2c585a606963da0400980440f47
Francisco Sanchez Oliver <spuzi@hotmail.es>
2024-12-02 08:43:56 +0100

    WIP Regression Metrics

--- ciencia_de_datos_desde_cero_oreilly/code/test_funciones_generales.py
+++ ciencia_de_datos_desde_cero_oreilly/code/test_funciones_generales.py
@@ -10,4 +10,31 @@ def test_add():
         assert str(e) == "vectors must be the same length"
 
 
-test_add()
+def test_subtract():
+    assert funciones_generales.subtract([1, 2, 3], [4, 5, 6]) == [-3, -3, -3]
+
+    try:
+        funciones_generales.subtract([1, 2, 3], [4, 5])
+    except Exception as e:
+        assert str(e) == "vectors must be the same length"
+
+
+def test_vector_sum():
+    assert funciones_generales.vector_sum(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    ) == [12, 15, 18]
+
+    try:
+        funciones_generales.vector_sum([[1, 2, 3], [1, 2, 3], [4, 5]])
+    except Exception as e:
+        assert str(e) == "vectors must be the same length"
+
+
+def test_scalar_multiply():
+    assert funciones_generales.scalar_multiply(c=2, v=[1, 2, 3]) == [2, 4, 6]
+
+
+def test_vector_mean():
+    assert funciones_generales.vector_mean(
+        [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    ) == [4, 5, 6]
